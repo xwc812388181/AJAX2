26c01b6643c2698aac1109ce3a19a8ecc8ff6596
xwc812388181 <64845019+xwc812388181@users.noreply.github.com>
2020-07-01 21:27:51 +0800

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,98 @@
+console.log("我是main.js");
+
+let n = 1;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const array = JSON.parse(request.response);
+      array.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+      n += 1;
+    }
+  };
+  request.send();
+};
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const object = JSON.parse(request.response);
+      myName.textContent = object.name;
+      myAge.textContent = object.age;
+      console.log(object);
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName("warning")[0].textContent;
+      console.log(text.trim());
+    }
+  };
+
+  request.send();
+};
+
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    console.log("成功了");
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {
+    console.log("失败了");
+  };
+  request.send();
+};
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    const script = document.createElement("script");
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+
+  request.onerror = () => {};
+  request.send();
+};
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "style.css");
+
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载失败");
+      }
+    }
+  };
+
+  request.send();
+};
